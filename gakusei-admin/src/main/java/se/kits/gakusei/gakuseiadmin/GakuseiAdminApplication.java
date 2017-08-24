b14ed484a67664af22ad530e8c623a1f5d45d360
Meddan <christoffer.medin@gmail.com>
2017-08-24 14:55:22 +0200

    Initialized project.

---
+++ gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/GakuseiAdminApplication.java
@@ -0,0 +1,12 @@
+package se.kits.gakusei.gakuseiadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GakuseiAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GakuseiAdminApplication.class, args);
+	}
+}
