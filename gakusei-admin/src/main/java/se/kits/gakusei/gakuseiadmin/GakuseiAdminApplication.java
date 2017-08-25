0389c86fe59db732118a336f76833a1dbf72d94d
Meddan <christoffer.medin@gmail.com>
2017-08-25 16:45:28 +0200

    Added componentscan in order to fix gakusei dependency.

--- gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/GakuseiAdminApplication.java
+++ gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/GakuseiAdminApplication.java
@@ -2,8 +2,10 @@ package se.kits.gakusei.gakuseiadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "se.kits.gakusei,se.kits.gakuseiadmin")
 public class GakuseiAdminApplication {
 
 	public static void main(String[] args) {
