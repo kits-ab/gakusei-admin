84acf36b414c7abf062f0ef787284beb1e3f4aff
Meddan <christoffer.medin@gmail.com>
2017-08-28 08:40:15 +0200

    Added example repository as well as example controller.

---
+++ gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/Controllers/BasicController.java
@@ -0,0 +1,20 @@
+package se.kits.gakusei.gakuseiadmin.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.kits.gakusei.gakuseiadmin.content.AdminFactRepository;
+
+
+@RestController
+public class BasicController {
+    @Autowired
+    private AdminFactRepository factRepository;
+    @RequestMapping("/hello")
+    public String hello() {
+    for (String s : factRepository.getAllFactTypes()) {
+        System.out.println(s);
+    }
+    return "hello worlds";
+    }
+}
