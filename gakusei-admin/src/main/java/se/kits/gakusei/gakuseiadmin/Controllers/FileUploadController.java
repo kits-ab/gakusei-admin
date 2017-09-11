82a0e3633fe615e687bc2c79e176894339faefec
Doverstav <pontus.doverstav@kits.se>
2017-09-11 14:41:26 +0200

    Moved repositories to controller, other changes to work better with CSVToDatabase

--- gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/Controllers/FileUploadController.java
+++ gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/Controllers/FileUploadController.java
@@ -1,25 +1,48 @@
 package se.kits.gakusei.gakuseiadmin.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import se.kits.gakusei.content.model.Nugget;
+import se.kits.gakusei.content.repository.BookRepository;
+import se.kits.gakusei.content.repository.LessonRepository;
+import se.kits.gakusei.content.repository.NuggetRepository;
+import se.kits.gakusei.content.repository.WordTypeRepository;
 import se.kits.gakusei.gakuseiadmin.utility.CSVToDatabase;
+
+import java.util.List;
 
 @RestController
 public class FileUploadController {
 
+    @Autowired
+    private LessonRepository lessonRepository;
+
+    @Autowired
+    private NuggetRepository nuggetRepository;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private WordTypeRepository wordTypeRepository;
+
     @PostMapping("/api/nugget/import/csv")
     public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file){
 
-        CSVToDatabase parser = new CSVToDatabase(file);
+        CSVToDatabase parser = new CSVToDatabase(file, lessonRepository, nuggetRepository, wordTypeRepository, bookRepository);
 
-        if(parser.parse()){
+        //try {
+            List<Nugget> nuggets = parser.parse();
             return ResponseEntity.ok().body(file.getName() + " was received!");
-        } else {
-            return ResponseEntity.badRequest().body("Malformed CSV");
-        }
+        //} catch(ParserFailureException e) {
+        //    return ResponseEntity.badRequest().body(e.getMessage());
+        //}
+
+        // TODO : Save parsed nuggets to database
 
     }
 
