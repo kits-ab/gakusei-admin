8297d6c2379d9d1602abb28db5656a166f00c7ab
Doverstav <pontus.doverstav@kits.se>
2017-09-11 14:38:32 +0200

    Created custom exception for parser errors

---
+++ gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/utility/ParserFailureException.java
@@ -0,0 +1,10 @@
+package se.kits.gakusei.gakuseiadmin.utility;
+
+public class ParserFailureException extends Exception {
+
+    public ParserFailureException() {}
+
+    public ParserFailureException(String message){
+        super(message);
+    }
+}
