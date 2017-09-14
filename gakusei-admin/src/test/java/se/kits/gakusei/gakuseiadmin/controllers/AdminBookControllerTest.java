9d682a9267a188cda8f880c457f3b8e82c837587
Doverstav <pontus.doverstav@kits.se>
2017-09-14 11:48:21 +0200

    Created test class, added headers for basic tests

---
+++ gakusei-admin/src/test/java/se/kits/gakusei/gakuseiadmin/controllers/AdminBookControllerTest.java
@@ -0,0 +1,28 @@
+package se.kits.gakusei.gakuseiadmin.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AdminBookControllerTest {
+
+
+    @Test
+    public void testBasicPUT(){
+
+    }
+
+    @Test
+    public void testBasicPOST(){
+
+    }
+
+    @Test
+    public void testBasicDELETE(){
+        
+    }
+
+}
