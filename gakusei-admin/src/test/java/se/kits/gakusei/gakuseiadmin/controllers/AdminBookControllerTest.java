32bd7d9292cdf12114758451d467a6351bdb082d
Doverstav <pontus.doverstav@kits.se>
2017-09-14 14:06:00 +0200

    Implemented basic test for PUT

--- gakusei-admin/src/test/java/se/kits/gakusei/gakuseiadmin/controllers/AdminBookControllerTest.java
+++ gakusei-admin/src/test/java/se/kits/gakusei/gakuseiadmin/controllers/AdminBookControllerTest.java
@@ -1,17 +1,60 @@
 package se.kits.gakusei.gakuseiadmin.controllers;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.ResponseEntity;
+import se.kits.gakusei.content.model.Book;
+import se.kits.gakusei.content.repository.BookRepository;
+import se.kits.gakusei.gakuseiadmin.Controllers.AdminBookController;
 
 import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AdminBookControllerTest {
 
+    @InjectMocks
+    AdminBookController adminBookController;
+
+    @Mock
+    BookRepository bookRepository;
+
+    private String oldTitle;
+    private String newTitle;
+    private String invalidTitle;
+
+    private Book testBook;
+
+    @Before
+    public void setUp(){
+
+        MockitoAnnotations.initMocks(this);
+
+        oldTitle = "Old title";
+        newTitle = "New title";
+        invalidTitle = "Invalid title";
+
+        testBook = new Book();
+        testBook.setTitle(oldTitle);
+
+    }
 
     @Test
     public void testBasicPUT(){
+
+        Mockito.when(bookRepository.findByTitle(oldTitle)).thenReturn(testBook);
+        testBook.setTitle(newTitle);
+        Mockito.when(bookRepository.save(testBook)).thenReturn(testBook);
+
+        ResponseEntity<Book> re = adminBookController.updateBookTitle(oldTitle, newTitle);
+
+        assertEquals(200, re.getStatusCodeValue());
+        assertEquals(testBook, re.getBody());
 
     }
 
@@ -22,7 +65,7 @@ public class AdminBookControllerTest {
 
     @Test
     public void testBasicDELETE(){
-        
+
     }
 
 }
