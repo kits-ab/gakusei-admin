0ac9dd756212a9af0fbb329c431900c90117b4ac
Doverstav <pontus.doverstav@kits.se>
2017-09-15 10:31:12 +0200

    Removed admin book repository

--- gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/content/AdminBookRepository.java
+++
@@ -1,7 +0,0 @@
-package se.kits.gakusei.gakuseiadmin.content;
-
-import org.springframework.data.repository.CrudRepository;
-import se.kits.gakusei.content.model.Book;
-
-public interface AdminBookRepository extends CrudRepository<Book, Long> {
-}
