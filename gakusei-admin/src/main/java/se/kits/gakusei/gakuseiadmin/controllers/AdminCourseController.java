27b1be7250a5ab87c5c2e9df5b2dd7b0cb92fe12
Doverstav <pontus.doverstav@kits.se>
2017-09-20 11:37:15 +0200

    Add @RequestMapping to methods in AdminCourseController

--- gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/controllers/AdminCourseController.java
+++ gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/controllers/AdminCourseController.java
@@ -1,7 +1,10 @@
 package se.kits.gakusei.gakuseiadmin.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import se.kits.gakusei.content.model.Course;
 import se.kits.gakusei.gakuseiadmin.content.AdminCourseRepository;
@@ -12,14 +15,29 @@ public class AdminCourseController {
     @Autowired
     AdminCourseRepository adminCourseRepository;
 
+    @RequestMapping(
+            value = "api/courses/create",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+    )
     public ResponseEntity<Course> createCourse(){
         return null;
     }
 
+    @RequestMapping(
+            value = "api/courses/update",
+            method = RequestMethod.PUT,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+    )
     public ResponseEntity<Course> updateCourse(){
         return null;
     }
 
+    @RequestMapping(
+            value = "api/courses/{courseID}/delete",
+            method = RequestMethod.DELETE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+    )
     public ResponseEntity<Course> deleteCourse(){
         return null;
     }
