6113bb273f4dfc18f77edf09f4e0488c58266199
Sofia Edström <sofia.edstrom@kits.se>
2017-09-25 09:55:45 +0200

    remove unused imports

--- gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/content/AdminQuizRepository.java
+++ gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/content/AdminQuizRepository.java
@@ -1,11 +1,7 @@
 package se.kits.gakusei.gakuseiadmin.content;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import se.kits.gakusei.content.model.Quiz;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public interface AdminQuizRepository extends CrudRepository<Quiz, Long> {
