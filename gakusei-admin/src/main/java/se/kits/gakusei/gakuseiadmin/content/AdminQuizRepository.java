a36498d2a070711e0a7d30d218ad852aa4cc2a8f
Sofia Edström <sofia.edstrom@kits.se>
2017-09-25 11:42:02 +0200

    remove unnecessary method

--- gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/content/AdminQuizRepository.java
+++ gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/content/AdminQuizRepository.java
@@ -2,11 +2,11 @@ package se.kits.gakusei.gakuseiadmin.content;
 
 import org.springframework.data.repository.CrudRepository;
 import se.kits.gakusei.content.model.Quiz;
+
 import java.util.List;
 
 public interface AdminQuizRepository extends CrudRepository<Quiz, Long> {
 
     List<Quiz> findByName(String name);
-    List<Quiz> findAll();
 
 }
