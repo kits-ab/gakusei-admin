828ef7cc687803fd5afa2a461b5c3d5e503810cc
Doverstav <pontus.doverstav@kits.se>
2017-09-29 16:49:02 +0200

    Add some security

--- gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/content/AdminUserRepository.java
+++ gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/content/AdminUserRepository.java
@@ -3,10 +3,12 @@ package se.kits.gakusei.gakuseiadmin.content;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 import se.kits.gakusei.user.model.User;
 
 public interface AdminUserRepository extends CrudRepository<User, String> {
 
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @Query("SELECT u FROM User u WHERE u.username LIKE CONCAT('%', :name, '%')")
     Iterable<User> findByNameContains(@Param("name") String name);
 }
