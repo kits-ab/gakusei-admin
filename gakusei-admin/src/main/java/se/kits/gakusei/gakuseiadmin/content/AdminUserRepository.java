7291849ac70115d59408e02073dc32a63af8e908
Doverstav <pontus.doverstav@kits.se>
2017-10-04 14:27:53 +0200

    Implement method for searching based on username and role

--- gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/content/AdminUserRepository.java
+++ gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/content/AdminUserRepository.java
@@ -11,4 +11,8 @@ public interface AdminUserRepository extends CrudRepository<User, String> {
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @Query("SELECT u FROM User u WHERE u.username LIKE CONCAT('%', :name, '%')")
     Iterable<User> findByNameContains(@Param("name") String name);
+
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @Query("SELECT u FROM User u WHERE u.username LIKE CONCAT('%', :name, '%') AND u.role LIKE CONCAT('%', :role, '%')")
+    Iterable<User> findByNameContainsFilterByRole(@Param("name") String name, @Param("role") String role);
 }
