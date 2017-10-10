fdca0a1096d803d7e4c20bc4138132f59b7ab5fc
Doverstav <pontus.doverstav@kits.se>
2017-10-10 09:59:04 +0200

    Implement tests for delete, change role, change password

---
+++ gakusei-admin/src/test/java/se/kits/gakusei/gakuseiadmin/unit/controllers/AdminUserControllerTest.java
@@ -0,0 +1,94 @@
+package se.kits.gakusei.gakuseiadmin.unit.controllers;
+
+import com.sun.org.apache.regexp.internal.RE;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import se.kits.gakusei.gakuseiadmin.content.AdminUserRepository;
+import se.kits.gakusei.gakuseiadmin.controllers.AdminUserController;
+import se.kits.gakusei.gakuseiadmin.tools.AdminTestTools;
+import se.kits.gakusei.user.model.Event;
+import se.kits.gakusei.user.model.User;
+import se.kits.gakusei.user.repository.EventRepository;
+
+import java.security.Principal;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AdminUserControllerTest {
+
+    @InjectMocks
+    private AdminUserController adminUserController;
+
+    @Mock
+    private AdminUserRepository adminUserRepository;
+
+    @Mock
+    private EventRepository eventRepository;
+
+    @Mock
+    private Principal principal;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    private User testUser;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        testUser = AdminTestTools.createUser();
+
+        Mockito.when(eventRepository.save(Mockito.any(Event.class))).thenReturn(null);
+        Mockito.when(principal.getName()).thenReturn(testUser.getUsername());
+        Mockito.when(passwordEncoder.encode(Mockito.any())).thenReturn("encoded" + testUser.getPassword());
+    }
+
+    @Test
+    public void searchUserFilterByRole() throws Exception {
+    }
+
+    @Test
+    public void resetPassword() throws Exception {
+        Mockito.when(adminUserRepository.findOne(testUser.getUsername())).thenReturn(testUser);
+        testUser.setPassword("newPassword");
+        Mockito.when(adminUserRepository.save(testUser)).thenReturn(testUser);
+
+        ResponseEntity<User> re = adminUserController.resetPassword(testUser, principal);
+
+        assertEquals(HttpStatus.OK, re.getStatusCode());
+        assertEquals(testUser, re.getBody());
+    }
+
+    @Test
+    public void changeRole() throws Exception {
+        Mockito.when(adminUserRepository.findOne(testUser.getUsername())).thenReturn(testUser);
+        testUser.setRole("newRole");
+        Mockito.when(adminUserRepository.save(testUser)).thenReturn(testUser);
+
+        ResponseEntity<User> re = adminUserController.changeRole(testUser, principal);
+
+        assertEquals(HttpStatus.OK, re.getStatusCode());
+        assertEquals(testUser, re.getBody());
+    }
+
+    @Test
+    public void deleteUser() throws Exception {
+        Mockito.when(adminUserRepository.exists(testUser.getUsername())).thenReturn(true);
+
+        ResponseEntity<User> re = adminUserController.deleteUser(testUser.getUsername(), principal);
+
+        assertEquals(HttpStatus.OK, re.getStatusCode());
+    }
+
+}
