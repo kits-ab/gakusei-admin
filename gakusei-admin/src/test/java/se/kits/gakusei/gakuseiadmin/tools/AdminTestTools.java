e261eeb955a625c7594aca6aa369671612db8a54
Doverstav <pontus.doverstav@kits.se>
2017-10-11 17:08:21 +0200

    Add util methods to AdminTestTools

--- gakusei-admin/src/test/java/se/kits/gakusei/gakuseiadmin/tools/AdminTestTools.java
+++ gakusei-admin/src/test/java/se/kits/gakusei/gakuseiadmin/tools/AdminTestTools.java
@@ -25,16 +25,39 @@ public class AdminTestTools {
         return course;
     }
 
-    public static User createUser() {
+    public static User createUser(String username, String role) {
         User user = new User();
-        user.setRole("ROLE_ADMIN");
-        user.setUsername("testUser");
+        user.setRole(role);
+        user.setUsername(username);
         user.setPassword("password");
         user.setEvents(null);
         user.setProgressTrackingList(null);
         user.setUsersLessons(null);
 
         return user;
+    }
+
+    public static ArrayList<User> createUsers() {
+        ArrayList<User> toReturn = new ArrayList<>();
+
+        toReturn.add(createUser("admin", "ROLE_ADMIN"));
+        toReturn.add(createUser("user", "ROLE_USER"));
+        toReturn.add(createUser("owner", "ROLE_ADMIN"));
+        toReturn.add(createUser("Alice", "ROLE_USER"));
+        toReturn.add(createUser("Bob", "ROLE_USER"));
+
+        return toReturn;
+    }
+
+    public static Iterable<User> matchingUsernameAndRole(Iterable<User> toTest, String name, String role) {
+        ArrayList<User> toReturn = new ArrayList<>();
+        for(User u : toTest) {
+            if (u.getUsername().toUpperCase().contains(name.toUpperCase())
+                    && u.getRole().toUpperCase().contains(role.toUpperCase())) {
+                toReturn.add(u);
+            }
+        }
+        return toReturn;
     }
 
     public static Book generateTestBook(String title) {
