b48065f7a141ac6d6416c82fbff94d4b30577d20
Doverstav <pontus.doverstav@kits.se>
2017-10-27 09:31:41 +0200

    Refactor code in attempt to clean up

--- gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/controllers/AdminQuizController.java
+++ gakusei-admin/src/main/java/se/kits/gakusei/gakuseiadmin/controllers/AdminQuizController.java
@@ -15,6 +15,7 @@ import se.kits.gakusei.gakuseiadmin.util.csv.CSVQuiz;
 import se.kits.gakusei.gakuseiadmin.util.FormValidator;
 import se.kits.gakusei.gakuseiadmin.util.AdminQuizHandler;
 import se.kits.gakusei.content.repository.QuizRepository;
+import se.kits.gakusei.util.ParserFailureException;
 import se.kits.gakusei.util.csv.CSV;
 import se.kits.gakusei.util.csv.CSVQuizNugget;
 
@@ -38,16 +39,12 @@ public class AdminQuizController {
 
     @RequestMapping(
             value = "/api/quizes/csv",
-            method = RequestMethod.POST,
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+            method = RequestMethod.POST
     )
     public ResponseEntity<String> importCsv(@RequestParam("quizName") String quizName,
                                             @RequestParam("quizDescription") String quizDescription,
                                             @RequestBody MultipartFile file) {
-        Quiz newQuiz = new Quiz();
-        newQuiz.setDescription(quizDescription);
-        newQuiz.setName(quizName);
-        newQuiz = quizRepository.save(newQuiz);
+        Quiz newQuiz = createNewQuiz(quizName, quizDescription);
 
         Map<String, List<String[]>> parseResult;
 
@@ -59,17 +56,59 @@ public class AdminQuizController {
 
         List<String[]> rows = parseResult.get("ROWS");
 
+        ArrayList<QuizNugget> quizNuggets = new ArrayList<>();
+        ArrayList<Iterable<IncorrectAnswers>> incorrectAnswers = new ArrayList<>();
+
         for(String[] row : rows) {
-            String[] rowWithQuizInfo = {quizName, quizDescription, row[0], row[1], row[2]};
-            CSVQuizNugget csvQuizNugget = new CSVQuizNugget(rowWithQuizInfo);
-            QuizNugget quizNugget = csvQuizNugget.getQuizNugget(newQuiz);
-            quizNugget = quizNuggetRepository.save(quizNugget);
-            Iterable<IncorrectAnswers> incorrectAnswers = csvQuizNugget.getIncorrectAnswers(quizNugget);
-            incorrectAnswers = incorrectAnswerRepository.save(incorrectAnswers);
+            try {
+                String[] rowWithQuizInfo = appendQuizInfo(quizName, quizDescription, row);
+
+                CSVQuizNugget csvQuizNugget = new CSVQuizNugget(rowWithQuizInfo);
+                QuizNugget quizNugget = csvQuizNugget.getQuizNugget(newQuiz);
+                Iterable<IncorrectAnswers> incorrectAnswer = csvQuizNugget.getIncorrectAnswers(quizNugget);
+
+                quizNuggets.add(quizNugget);
+                incorrectAnswers.add(incorrectAnswer);
+            } catch (ParserFailureException pfe) {
+                return new ResponseEntity<String>("Error parsing row: " + pfe.getMessage(), HttpStatus.BAD_REQUEST);
+            }
         }
+
+        saveQuiz(newQuiz, quizNuggets, incorrectAnswers);
 
         return new ResponseEntity<String>("File successfully uploaded", HttpStatus.CREATED);
 
+    }
+
+    private Quiz createNewQuiz(String name, String description){
+        Quiz toReturn = new Quiz();
+        toReturn.setName(name);
+        toReturn.setDescription(description);
+
+        return toReturn;
+    }
+
+    private String[] appendQuizInfo(String name, String description, String[] quizNuggetRow){
+        String[] appendedRow = new String[2+quizNuggetRow.length];
+
+        appendedRow[0] = name;
+        appendedRow[1] = description;
+
+        for(int i = 0; i < quizNuggetRow.length; i++){
+            appendedRow[i+2] = quizNuggetRow[i];
+        }
+
+        return appendedRow;
+    }
+
+    private void saveQuiz(Quiz quiz,
+                          ArrayList<QuizNugget> quizNuggets,
+                          ArrayList<Iterable<IncorrectAnswers>> incorrectAnswers){
+        quizRepository.save(quiz);
+        quizNuggetRepository.save(quizNuggets);
+        for(Iterable<IncorrectAnswers> ia : incorrectAnswers){
+            incorrectAnswerRepository.save(ia);
+        }
     }
 
     @RequestMapping(
